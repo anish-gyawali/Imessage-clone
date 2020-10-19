0e9cec8282c978a6633e8a541c0e6cb40410c4b8
anishgyawali <anish.gyawali@selu.edu>
2020-10-19 12:20:28 -0500

    added sidebar

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './App.css';
+import Imessage from './Imessage';
+
+function App() {
+  return (
+    //BEM naming convenction
+    <div className="app">
+   
+     <Imessage/>
+    </div>
+  );
+}
+
+export default App;
